6e46394853f2219ab301b1a28458df6cfd732641
bilalfaisal31 <bilal.faisal31@gmail.com>
2021-03-07 01:49:40 +0500

    Update script.js
    Added functionality to check email and length validation and confirm password.

--- Project1/script.js
+++ Project1/script.js
@@ -18,6 +18,12 @@ function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+function checkEmail(input) {
+  if (!validateEmail(input.value) && input.value !== "") {
+    showError(input, `${upper(input.id)} is Invalid.`);
+  }
+}
 function checkRequired(inputArray) {
   inputArray.forEach(function (input) {
     if (input.value === "") {
@@ -31,8 +37,28 @@ function upper(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < min && input.value.length > 0) {
+    showError(input, `${upper(input.id)} must be at least ${min} characters.`);
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${upper(input.id)} must be at most of ${max} characters.`
+    );
+  }
+}
+function checkPassword(input1, input2) {
+  if (input1.value !== input2.value && input2.value !== "") {
+    showError(input2, "Passwords don't match");
+  }
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkEmail(email);
+  checkLength(username, 3, 25);
+  checkLength(password, 6, 30);
+  checkPassword(password, password2);
 });
