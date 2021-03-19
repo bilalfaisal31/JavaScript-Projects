55a3d438023df5dde4d9c74bf37ed28284ae6a63
bilalfaisal31 <bilal.faisal31@gmail.com>
2021-03-20 03:47:33 +0500

    Completed Project2 javascript
    - update UI for selected seats
    - save data to local storage
    - build UI with saved data from local storage

--- Project2/script.js
+++ Project2/script.js
@@ -6,6 +6,26 @@ const total = document.querySelector(".price");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = Number(movieSelect.value);
 
+populatedUI();
+// Pull data to local storage to build UI
+function populatedUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log(selectedSeats);
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
@@ -20,11 +40,32 @@ container.addEventListener("click", (e) => {
 function updateSelectedSeats() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const countSelectedSeats = selectedSeats.length;
+
+  const seatsIndex = [...selectedSeats].map((singleSeat) =>
+    [...seats].indexOf(singleSeat)
+  );
+
+  //   const seatsIndex = [...selectedSeats].map(function (singleSeat) {
+  //     return [...seats].indexOf(singleSeat);
+  //   });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   count.innerText = countSelectedSeats;
   total.innerText = ticketPrice * countSelectedSeats;
 }
 
+// func to save selested movie and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedSeats();
 });
+
+// calculate initial no of seats and total price
+updateSelectedSeats();
