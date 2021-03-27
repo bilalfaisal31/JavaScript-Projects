7444174c3813e285bbcf0cc0e32f8346c83e40ef
bilalfaisal31 <bilal.faisal31@gmail.com>
2021-03-27 21:07:14 +0500

    JS functionalities added
    A little improvement in html and css files while main work is done on script.js

--- Project3/script.js
+++ Project3/script.js
@@ -1,10 +1,50 @@
 "use strict";
 
-const score1 = document.querySelector("#score1");
-const score2 = document.querySelector("#score2");
-const dice = document.querySelector("#dice");
+const score_0 = document.querySelector("#score0");
+const score_1 = document.querySelector("#score1");
+const diceElement = document.querySelector("#dice");
+const btn_reset = document.querySelector("#btn-reset");
+const btn_dice = document.querySelector("#btn-dice");
+const btn_roll = document.querySelector("#btn-hold");
+const current_score_p0 = document.querySelector("#current-score-p0");
+const current_score_p1 = document.querySelector("#current-score-p1");
+const player_1 = document.querySelector(".player-0");
+const player_2 = document.querySelector(".player-1");
 
-score1.innerText = 0;
-score2.innerText = 0;
+score_0.innerText = 0;
+score_1.innerText = 0;
+current_score_p0.innerText = 0;
+let currentScore = Number(current_score_p0.innerText);
+let activePlayer = 0;
+const scores = [0, 0];
 
-dice.classList.add("hidden");
+diceElement.classList.add("hidden");
+
+btn_dice.addEventListener("click", function () {
+  // Generate a random no
+  const diceNo = Math.trunc(Math.random() * 6) + 1;
+
+  // Display Dice
+  diceElement.classList.remove("hidden");
+  diceElement.src = `dice-${diceNo}.png`;
+
+  // Check if 1
+  if (diceNo !== 1) {
+    // Add to current score
+    currentScore += diceNo;
+    document.querySelector(
+      `#current-score-p${activePlayer}`
+    ).innerText = currentScore;
+  } else {
+    // Switch player
+    currentScore = 0;
+    document.querySelector(
+      `#current-score-p${activePlayer}`
+    ).innerText = currentScore;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player_1.classList.toggle("active-player");
+    player_2.classList.toggle("active-player");
+  }
+});
