962e929567717969e07b45e672271172e0052180
bilalfaisal31 <bilal.faisal31@gmail.com>
2021-03-27 07:29:02 +0500

    scrpit.js
    stated work on script.js

---
+++ Project3/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const score1 = document.querySelector("#score1");
+const score2 = document.querySelector("#score2");
+const dice = document.querySelector("#dice");
+
+score1.innerText = 0;
+score2.innerText = 0;
+
+dice.classList.add("hidden");
